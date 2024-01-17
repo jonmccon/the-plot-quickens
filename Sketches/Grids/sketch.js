83242c05b18d5e2472417188845702211e28ebd9
Jonny McConnell <jonmccon@gmail.com>
2024-01-17 11:17:43 -0800

    gonna have to work with offsets somewhere

--- Sketches/Grids/sketch.js
+++ Sketches/Grids/sketch.js
@@ -18,12 +18,14 @@ function createGrid() {
   let cols = (width - 2 * inset) / gridSize;
 
   // Draw the rotated grid lines
-  let rotations = 1; // Number of rotations
-  let rotationIncrement = 15; // Degrees to rotate each time
+  let rotations = 10; // Number of rotations
+  let rotationIncrement = 10; // Degrees to rotate each time
   for (let r = 0; r < rotations; r++) {
     push(); // Save the current transformation matrix
     translate(width / 2, height / 2); // Move the origin to the center of the canvas
     rotate(radians(r * rotationIncrement)); // Rotate by the current rotation
+
+    stroke((r * 36) % 360, 50, 50); // Change this line to set the color as you want
 
     // Draw the grid lines
     for (let i = 0; i <= rows; i++) {
@@ -31,15 +33,22 @@ function createGrid() {
       for (let j = 0; j <= cols; j++) {
         let x = j * gridSize - width / 2 + inset;
         let y = i * gridSize - height / 2 + inset;
-        let noiseValue = noise(x * 0.1, y * 0.1); // Generate noise based on the x and y coordinates
-        let noisyY = y + map(noiseValue, 0, 1, -gridSize, gridSize); // Map the noise value to a range that fits the grid size
-        vertex(x, noisyY);
+        // let noiseValue = noise(x * 0.1, y * 0.1); // Generate noise based on the x and y coordinates
+        // let noisyY = y + map(noiseValue, 0, 1, -gridSize, gridSize); // Map the noise value to a range that fits the grid size
+        vertex(x, y);
       }
       endShape();
     }
 
     pop(); // Restore the transformation matrix
   }
+    // Calculate the radius of the circle
+    let circleRadius = dist(width / 2, height / 2, width - inset, height - inset);
+
+    // Draw the circle
+    noFill();
+    stroke(0); // Set the color of the circle
+    circle(width / 2, height / 2, circleRadius * 2);
 }
 
 // Download SVG when clicked
